e0b763186bc0644ad864fe52c08d13fa4785579c
Erackson Souza <souza.erackson@gmail.com>
2026-01-12 20:25:45 -0300

    feat: implement Login and SignUp pages with form components and routing

--- src/routes/cadastro.tsx
+++ src/routes/cadastro.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import SignUpPage from "@/pages/Auth/SignUp";
+import SignUpPage from "@/pages/_auth/SignUp";
 
 export const Route = createFileRoute("/cadastro")({
   component: SignUpPage,
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import LoginPage from "@/pages/Auth/Login";
+import LoginPage from "@/pages/_auth/SignIn";
 
 export const Route = createFileRoute("/login")({
   component: LoginPage,

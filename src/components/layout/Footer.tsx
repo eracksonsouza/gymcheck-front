e793b0963e8255e84eeedfb5701718b6e5adc30d
Erackson Souza <souza.erackson@gmail.com>
2026-01-12 20:27:43 -0300

    feat: improve Footer layout and enhance Logo component with Link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,9 +2,9 @@ import Logo from "./Logo";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-between px-6 py-4 border-t border-white/10 bg-[#0f1116]/95 backdrop-blur-md">
-      <div className="flex justify-between w-full">
-        <div className="flex justify-between w-full h-[10vh] items-center">
+    <footer className="flex items-center justify-between px-6 py-4 border-white/10 bg-[#0f1116]/95 backdrop-blur-md">
+      <div className="flex flex-col md:flex-row justify-between w-full">
+        <div className="flex flex-col md:flex-row md:justify-between w-full items-center gap-2 md:gap-0 md:h-[10vh]">
           <Logo />
           <p className="text-sm text-white">
             © 2025 GymCheck. Todos os direitos reservados.
--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,12 +1,15 @@
 import logoCheckGym from '@/assets/logo-checkgym.png';
+import { Link } from '@tanstack/react-router';
 
 const Logo = () => {
     return ( 
         <div className="flex items-center gap-2">
+            <Link to="/" className="flex items-center gap-2">
             <div className="h-20 w-20 rounded-full flex items-center justify-center shadow-lg">
                 <img src={logoCheckGym} alt="GymCheck Logo" />
             </div>
             <span className="text-xl font-bold text-white">GymCheck</span>
+            </Link>
         </div>
      );
 }
